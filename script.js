6a9d15e958d6999423635765bf4562ca05185dff
Pier.W <factory666@live.it>
2019-02-22 16:57:39 +0100

    Refixed

--- script.js
+++ script.js
@@ -5,24 +5,23 @@ var data = [
   {"Nome" : "Fabio", "Cognome" : "Basile", "Età" : 30},
   {"Nome" : "Carlo", "Cognome" : "Rubino", "Età" : 22}
 ];
-var nome;
-var cognome;
-var età;
+
 /////Chiedo dati al cliente
 var iname = prompt("Dammi un nome: ");
 var isurname = prompt("Dammi un cognome: ");
 var iage = prompt("Dammi un Età: ");
-data.Nome = iname;
-data.Cognome = isurname;
-data.Età = iage;
-data.push({Nome: iname, Cognome: isurname, Età: iage});
+// data.Nome = iname;
+// data.Cognome = isurname;
+// data.Età = Number(iage);
+data.push({Nome: iname, Cognome: isurname, Età: Number(iage)});
 
 for (var i = 0; i < data.length; i++) {
-  nome = document.getElementById("studenti").innerHTML += "Nome: " + data[i].Nome + "<br>";
-  cognome = document.getElementById("studenti").innerHTML += "Cognome: " + data[i].Cognome + "<br>";
-  età = document.getElementById("studenti").innerHTML += "Età: " + data[i].Età + "<br><br>"
+ document.getElementById("studenti").innerHTML += "Nome: " + data[i].Nome + "<br>";
+ document.getElementById("studenti").innerHTML += "Cognome: " + data[i].Cognome + "<br>";
+ document.getElementById("studenti").innerHTML += "Età: " + data[i].Età + "<br><br>"
 }
 // return; Funziona anche senza return
+console.log(data);
 }
 
 dataStudents();
